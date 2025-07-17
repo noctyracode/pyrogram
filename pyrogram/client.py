3a68b17632f33ef23f26a79890e7fb4308d26ac1
noctyracode <admin@noctyra.web.id>
2025-07-17 22:31:36 +0700

    Update client.py

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -184,8 +184,8 @@ class Client(Methods):
             Defaults to 1.
     """
 
-    APP_VERSION = f"BeeVerse {__version__}"
-    DEVICE_MODEL = f"MasterHereXD"
+    APP_VERSION = f"CiroXL {__version__}"
+    DEVICE_MODEL = f"NoctyraCode"
     SYSTEM_VERSION = f"Ubuntu 22.0.4"
 
     LANG_CODE = "en"
